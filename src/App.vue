9861beb3525779cccc074ba9a030da8e7c30cbab
cairq <cairq@tongbaninfo.com>
2024-03-24 22:13:30 +0800

    20240324

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
+<!--
+ * @Author: cairq cairq@tongbaninfo.com
+ * @Date: 2024-03-24 10:43:09
+ * @LastEditors: cairq cairq@tongbaninfo.com
+ * @LastEditTime: 2024-03-24 17:42:40
+ * @FilePath: \icon-wallp\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup>
 </script>
 
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <span class="material-icons">pie_chart</span> <!-- Filled -->
+    <span class="material-icons-outlined">pie_chart</span> <!-- Outlined -->
+    <span class="material-icons-round">pie_chart</span> <!-- Round -->
+    <span class="material-icons-sharp">pie_chart</span> <!-- Sharp -->
+    <span class="material-icons-two-tone">pie_chart</span> <!-- Two Tone -->
+  </div>
 </template>
