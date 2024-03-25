eb6f958f743824a80b79c769cda5be282ff681bb
蔡荣诠 <cairq@tongbaninfo.com>
2024-03-25 20:54:22 +0800

    20240325

--- src/components/rightContent.vue
+++ src/components/rightContent.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: 蔡荣诠 cairq@tongbaninfo.com
+ * @Date: 2024-03-25 16:22:51
+ * @LastEditors: 蔡荣诠 cairq@tongbaninfo.com
+ * @LastEditTime: 2024-03-25 20:41:51
+ * @FilePath: /icon-wallp/src/components/rightContent.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts">
 import { inject } from 'vue'
 
@@ -6,5 +14,8 @@ const bgName = inject('bgName')
 </script>
 
 <template>
-  <div id="right-content"></div>
+  <div id="right-content">
+    {{ iconList }}
+    {{ bgName }}
+  </div>
 </template>
